8a10320a98705b7f2eea44217fa72852d7465728
Chauncy <liu5831835@163.com>
2021-06-12 08:52:19 +0800

    new

---
+++ src/test/java/com/example/spring/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.spring;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception{
+        mockMvc.perform(get("/"))
+                .andExpect(status().is(200))
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to...")));
+    }
+}
